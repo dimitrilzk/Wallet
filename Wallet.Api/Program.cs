b3b68cfbfeb6610b1b716536d02a9b7f2e5612f7
dimitri <dimitrilazuka95@gmail.com>
2026-03-09 07:50:35 +0100

    config db context and identity in program

--- Wallet.Api/Program.cs
+++ Wallet.Api/Program.cs
@@ -1,11 +1,32 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
+using Wallet.Api.Domain.Entities;
+using Wallet.Api.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+//DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+//Identity
+builder.Services.AddIdentityCore<AppUser>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequiredLength = 8;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+})
+    .AddRoles<IdentityRole<Guid>>()//adding roles
+    .AddEntityFrameworkStores<AppDbContext>()//identity using dbContext to save users/roles
+    .AddDefaultTokenProviders();//reset passworda, 2FA
+
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
