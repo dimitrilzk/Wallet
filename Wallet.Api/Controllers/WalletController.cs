03b370cd71a7580acbbf97d312ac43944017d07a
dimitri <dimitrilazuka95@gmail.com>
2026-03-27 07:56:16 +0100

    wip wallet controller

---
+++ Wallet.Api/Controllers/WalletController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Wallet.Api.DTOs.Wallet;
+
+namespace Wallet.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WalletController : ControllerBase
+    {
+        public WalletController()
+        {
+            
+        }
+
+        [Authorize]
+        [HttpPost("current")]
+        [ProducesResponseType(typeof(void), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<IActionResult> GetOrCreateWallet([FromBody] WalletRequestDto request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+
+        }
+    }
+}
---
+++ Wallet.Api/DTOs/Wallet/WalletRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Wallet.Api.DTOs.Wallet
+{
+    public sealed class WalletRequestDto
+    {
+        [Required]
+        public int Year { get; set; }
+    }
+}
