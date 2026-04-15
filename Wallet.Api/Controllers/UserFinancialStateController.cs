7fd4d539e4fdd29b7740a7304586b2f635c5255f
dimitri <dimitrilazuka95@gmail.com>
2026-04-15 07:48:27 +0200

    wip financial state

---
+++ Wallet.Api/Controllers/UserFinancialStateController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Wallet.Api.Controllers
+{
+    [ApiController]
+    [Route("api/financial-state")]
+    public class UserFinancialStateController : ControllerBase
+    {
+        public UserFinancialStateController()
+        {
+            
+        }
+    }
+}
