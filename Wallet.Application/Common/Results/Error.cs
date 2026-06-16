5fe4edc997eebf753767e478b837b60f67e8bf79
dimitri <dimitrilazuka95@gmail.com>
2026-06-16 07:58:44 +0200

    wip errors

---
+++ Wallet.Application/Common/Results/Error.cs
@@ -0,0 +1,15 @@
+﻿namespace Wallet.Application.Common.Results
+{
+    public class Error
+    {
+        public Error(string code, string message, ErrorType errorType)
+        {
+            Code = code;
+            Message = message;
+            ErrorType = errorType;
+        }
+        public string Code { get; init; }
+        public string Message { get; init; }
+        public ErrorType ErrorType { get; init; }
+    }
+}
---
+++ Wallet.Application/Common/Results/ErrorType.cs
@@ -0,0 +1,12 @@
+﻿namespace Wallet.Application.Common.Results
+{
+    public enum ErrorType
+    {
+        Validation, //400
+        Unauthorized, //401
+        Forbidden, //403
+        NotFound, //404
+        Conflict, //409
+        Unexpected //500
+    }
+}
