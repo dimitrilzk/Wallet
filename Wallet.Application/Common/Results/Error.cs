32062c1ed23c836f3dd48b31d1ae91b1a5f9d075
dimitri <dimitrilazuka95@gmail.com>
2026-06-18 08:01:33 +0200

    wip errors

--- Wallet.Application/Common/Results/Error.cs
+++ Wallet.Application/Common/Results/Error.cs
@@ -2,14 +2,44 @@
 {
     public class Error
     {
-        public Error(string code, string message, ErrorType errorType)
+        private Error(string code, string message, ErrorType errorType)
         {
             Code = code;
             Message = message;
-            ErrorType = errorType;
+            Type = errorType;
         }
         public string Code { get; init; }
         public string Message { get; init; }
-        public ErrorType ErrorType { get; init; }
+        public ErrorType Type { get; init; }
+
+        public static Error Unexpected(string message)
+        {
+            return new Error("Unexpected", message, ErrorType.Unexpected);
+        }
+
+        public static Error Validation(string code, string message)
+        {
+            return new Error(code, message, ErrorType.Validation);
+        }
+
+        public static Error Unauthorized(string code, string message)
+        {
+            return new Error(code, message, ErrorType.Unauthorized);
+        }
+
+        public static Error Forbidden(string code, string message)
+        {
+            return new Error(code, message, ErrorType.Forbidden);
+        }
+
+        public static Error NotFound(string code, string message)
+        {
+            return new Error(code, message, ErrorType.NotFound);
+        }
+
+        public static Error Conflict(string code, string message)
+        {
+            return new Error(code, message, ErrorType.Conflict);
+        }
     }
 }
